4aa25e50cce8f5e7baa0c9b59cf225e441d4d5db
Rick Meels <rick.meels@gmail.com>
2024-07-14 22:26:07 +0200

    Clean project

--- app/frontend/src/app/app.component.ts
+++ app/frontend/src/app/app.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HomeComponent } from './components/home/home.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HomeComponent
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Tool of Tools';
+  title = 'frontend';
 }
